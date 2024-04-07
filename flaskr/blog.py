743c4050fa340609fbe64a11faf10dfa3478e00b
SamuelAirs <sdayers@mail.fhsu.edu>
2024-04-07 13:41:48 -0500

    created html and updating blog.py

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -44,3 +44,13 @@ def create():
             return redirect(url_for('blog.index'))
 
     return render_template('blog/create.html')
+
+
+@bp.route('/<int:id>/delete', methods=('POST',))
+@login_required
+def delete(id):
+    get_post(id)
+    db = get_db()
+    db.execute('DELETE FROM post WHERE id = ?', (id,))
+    db.commit()
+    return redirect(url_for('blog.index'))
